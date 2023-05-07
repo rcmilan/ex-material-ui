e0e2c4e5eb1603df0803e3f6de7308a69a709276
rcmilan <13418519+rcmilan@users.noreply.github.com>
2023-05-07 12:55:13 -0300

    content

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -3,7 +3,9 @@ import FirstContent from "../../components/pages/home/firstContent";
 
 function Home() {
   return (
-    <Box sx={{ width: "100%", height: "100vh", overflowY: "auto" }}>
+    <Box sx={{ width: "100%", height: "100vh" }}>
+      <FirstContent />
+      <FirstContent />
       <FirstContent />
       {/* Add more content below this component */}
     </Box>
