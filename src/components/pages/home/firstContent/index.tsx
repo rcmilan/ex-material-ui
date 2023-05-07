fe2ba15ba3b3e5cf935e074db99fb2f2a1005087
rcmilan <13418519+rcmilan@users.noreply.github.com>
2023-05-07 13:18:29 -0300

    refac

--- src/components/pages/home/firstContent/index.tsx
+++ src/components/pages/home/firstContent/index.tsx
@@ -1,8 +1,9 @@
-import { Grid, Skeleton, Box, Typography } from "@mui/material";
+import { Grid, Skeleton, Box, Typography, useTheme } from "@mui/material";
+import { RightContentProps } from "./types";
 
-const FirstContent = () => (
-  <Grid container sx={{ height: "100%" }}>
-    <Grid item xs={8} sx={{ height: "50%", p: 0.5 }}>
+const LeftContent = () => {
+  return (
+    <>
       <Skeleton
         variant="rectangular"
         sx={{ height: "100%", borderRadius: 1 }}
@@ -11,32 +12,64 @@ const FirstContent = () => (
         variant="rectangular"
         sx={{ height: "100%", borderRadius: 1 }}
       />
-    </Grid>
-    <Grid item xs={4} sx={{ height: "50%" }}>
-      <Grid container direction="column" sx={{ height: "100%", p: 0.5 }}>
-        <Grid item sx={{ height: "80%" }}>
-          <Skeleton
-            variant="rectangular"
-            sx={{ height: "100%", borderRadius: 1 }}
-          />
-        </Grid>
-        <Grid item sx={{ height: "20%", p: 0.5 }}>
-          <Box
-            sx={{
-              height: "100%",
-              display: "flex",
-              alignItems: "center",
-              justifyContent: "center",
-            }}
-          >
-            <Typography variant="subtitle1" sx={{ fontWeight: "bold" }}>
-              Some text
-            </Typography>
-          </Box>
-        </Grid>
+    </>
+  );
+};
+
+const RightContent = ({ theme }: RightContentProps) => {
+  const UpperContent = () => (
+    <Skeleton variant="rectangular" sx={{ height: "100%", borderRadius: 1 }} />
+  );
+
+  const LowerContent = () => (
+    <Box
+      sx={{
+        height: "100%",
+        display: "flex",
+        alignItems: "baseline",
+        justifyContent: "center",
+      }}
+    >
+      <Typography variant="subtitle1">
+        <span
+          style={{
+            color: theme.palette.primary.main,
+            fontWeight: "bold",
+          }}
+        >
+          Lorem ipsum dolor sit amet
+        </span>
+        , consectetur adipiscing elit. Curabitur at interdum augue. Duis aliquam
+        ultrices ex, nec feugiat ex vulputate sed.
+      </Typography>
+    </Box>
+  );
+
+  return (
+    <Grid container direction="column" sx={{ height: "100%" }}>
+      <Grid item sx={{ height: "80%", p: 0.5 }}>
+        <UpperContent />
+      </Grid>
+      <Grid item sx={{ height: "20%", p: 0.5 }}>
+        <LowerContent />
       </Grid>
     </Grid>
-  </Grid>
-);
+  );
+};
+
+const FirstContent = () => {
+  const theme = useTheme();
+
+  return (
+    <Grid container sx={{ height: "100%" }}>
+      <Grid item xs={8} sx={{ height: "50%", p: 0.5 }}>
+        <LeftContent />
+      </Grid>
+      <Grid item xs={4} sx={{ height: "50%" }}>
+        <RightContent theme={theme} />
+      </Grid>
+    </Grid>
+  );
+};
 
 export default FirstContent;
